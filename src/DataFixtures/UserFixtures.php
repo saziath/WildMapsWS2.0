0b1f36d11e2432951dd1ae29a3406512f0de6d0a
saziath <saziath@users.noreply.github.com>
2019-05-24 00:36:57 +0200

    feat : login

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class UserFixtures extends Fixture
+{
+
+    /**
+     * @var UserPasswordEncoderInterface
+     */
+    private $encoder;
+
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+
+
+        $this->encoder = $encoder;
+    }
+
+
+
+    public function load(ObjectManager $manager)
+    {
+        // $product = new Product();
+        // $manager->persist($product);
+        //php bin/console doctrine:fixtures:load --apend
+        $user =new User();
+
+        $user->setusername('demo');
+        $user->setPassword($this->encoder->encodePassword($user,'demo'));
+
+        $manager->persist($user);
+
+        $manager->flush();
+    }
+}
